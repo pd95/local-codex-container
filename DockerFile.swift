e5e086c8dad4c4877c549d6cb13fdaf60e6b73db
Philipp <pd95@users.noreply.github.com>
2025-12-28 00:18:41 +0100

    Adding more tools (jq, ripgrep) fix git defaultBranch configuration

--- DockerFile.swift
+++ DockerFile.swift
@@ -4,7 +4,7 @@ FROM docker.io/swift:latest
 WORKDIR /workdir
 
 RUN apt-get update
-RUN apt-get -y install npm socat file curl
+RUN apt-get -y install npm file curl ripgrep jq python-is-python3
 RUN npm i -g @openai/codex 
 
 COPY config.toml /root/.codex/
@@ -12,6 +12,6 @@ COPY config.toml /root/.codex/
 # Configure git
 RUN git config --global user.email "codex@localhost"
 RUN git config --global user.name "Codex CLI"
-RUN git config --global init.defaultBranch main
+RUN git config --global init.defaultBranch "main"
 
 CMD codex --dangerously-bypass-approvals-and-sandbox --cd /workdir
